a6261ff2910106b7bbea529ab6ff4b1e429aad6c
Elena Larina <elena.larina.1@gmail.com>
2017-05-21 17:47:43 +0300

    Change perspective id

--- src/first/rcp/application/ApplicationWorkbenchAdvisor.java
+++ src/first/rcp/application/ApplicationWorkbenchAdvisor.java
@@ -6,7 +6,7 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	private static final String PERSPECTIVE_ID = "first.rcp.application.perspective"; //$NON-NLS-1$
+	private static final String PERSPECTIVE_ID = "first.rcp.application.Perspective"; //$NON-NLS-1$
 
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
